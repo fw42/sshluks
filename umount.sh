6c3e614d643c96ee5e50dca7c20ac11366cd9881
Florian Weingarten <flo@hackvalue.de>
2012-12-19 15:06:35 +0100

    umount.sh sshfs

--- umount.sh
+++ umount.sh
@@ -16,6 +16,7 @@ checkroot
 MOUNT_FS="$1"
 
 msg_status "Unmounting filesystem on \"$MOUNT_FS\" (\"$MAPPER/$CRYPTNAME\")..."
+sync
 umount $MOUNT_FS || die
 
 msg_status "Closing crypto container..."
@@ -25,3 +26,16 @@ cryptsetup luksClose $CRYPTNAME || die
 FILE=$(losetup $LOOP | awk {'print $3'} | sed "s/^(//" | sed "s/)$//")
 msg_status "Unmounting container file (\"$FILE\")..."
 losetup -d $LOOP
+
+MOUNT_SSHFS=$(stat -c "%m" $FILE)
+if [ "$(findmnt $MOUNT_SSHFS -n -o FSTYPE)" == "fuse.sshfs" ]
+then
+	msg_status "Removing filesystem lock..."
+	rm -f $(dirname $FILE)/lock
+
+	msg_status "Unmounting sshfs (\"$MOUNT_SSHFS\")..."
+	sync
+	umount $MOUNT_SSHFS
+fi
+
+msg_status "Done."
